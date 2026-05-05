f04c3b645c1c1a42ba20e2eee02f44f9caaa7c6a
Baek Jihoon <133375485+Chuseok22@users.noreply.github.com>
2026-05-05 17:32:46 +0900

    feat: features 기반 폴더 구조 및 최소 홈페이지 구성

---
+++ src/features/home/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import styles from './HomePage.module.css'
+
+export default function HomePage() {
+  return (
+    <main className={styles.container}>
+      <h1>chuseok22</h1>
+    </main>
+  )
+}
