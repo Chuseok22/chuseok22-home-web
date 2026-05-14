622a333099d391f9ea6b58c6bce7aac1fb67dbd3
Baek Jihoon <133375485+Chuseok22@users.noreply.github.com>
2026-05-14 19:47:38 +0900

    feat: HomePage 섹션 조립 완료

--- src/features/home/pages/HomePage.tsx
+++ src/features/home/pages/HomePage.tsx
@@ -1,9 +1,15 @@
-import styles from './HomePage.module.css'
+import HeroSection from '../components/HeroSection/HeroSection'
+import LinksSection from '../components/LinksSection/LinksSection'
+import ProjectsSection from '../components/ProjectsSection/ProjectsSection'
+import TechStackSection from '../components/TechStackSection/TechStackSection'
 
 export default function HomePage() {
   return (
-    <main className={styles.container}>
-      <h1>chuseok22</h1>
-    </main>
+    <>
+      <HeroSection />
+      <ProjectsSection />
+      <TechStackSection />
+      <LinksSection />
+    </>
   )
 }
