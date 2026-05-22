6e39ca2229b02c311b32f80228bd248aabf18c66
Baek Jihoon <133375485+Chuseok22@users.noreply.github.com>
2026-05-23 00:59:55 +0900

    팀_프로젝트_채널_구성_변경 : feat : TeamProjectsChannel에 ProjectCard 적용 https://github.com/Chuseok22/chuseok22-home-web/issues/7

--- src/features/projects/components/TeamProjectsChannel.tsx
+++ src/features/projects/components/TeamProjectsChannel.tsx
@@ -1,19 +1,12 @@
-import EmbedCard from '../../../shared/components/EmbedCard/EmbedCard'
 import ChannelSection from '../../../shared/components/ChannelSection/ChannelSection'
+import ProjectCard from './ProjectCard/ProjectCard'
 import { teamProjects } from '../constants/projectsData'
 
 export default function TeamProjectsChannel() {
   return (
     <ChannelSection title="🤝 팀프로젝트">
       {teamProjects.map((project) => (
-        <EmbedCard
-          key={project.id}
-          accentColor={project.accentColor}
-          title={project.title}
-          titleHref={project.titleHref}
-          description={project.description}
-          tags={[...project.tags, project.status]}
-        />
+        <ProjectCard key={project.id} project={project} />
       ))}
     </ChannelSection>
   )
